ddf088c1e30b74553ca81c13cc71d6f93df8ea5e
nascilino <nascilino@gmail.com>
2022-04-25 21:21:28 -0300

    make it better

--- projeto 2/src/routes/matrix.routes.js
+++ projeto 2/src/routes/matrix.routes.js
@@ -1,6 +1,11 @@
 const route = require("express").Router();
 const controllerMatrix = require("../controllers/matrix.controller");
 // const {validId, validObjectBody } = require('../middlewares/person.middleware');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../../swagger.json');
+
+route.use('/api-docs', swaggerUi.serve);
+route.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 route.get("/todos", controllerMatrix.findAllMatrixController);
 route.get("/person/:id", controllerMatrix.findByIdPersonController);
